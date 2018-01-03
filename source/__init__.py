0195d95038bb104c41b4d38d3806c24bd0d7cacc
mjarosie <jarosiewicz.maciek@gmail.com>
2018-01-03 20:30:30 +0000

    Fixed the DeprecationWarning for Flask distribution (to be fixed in the next version)

--- source/__init__.py
+++ source/__init__.py
@@ -10,6 +10,9 @@ from source.configs import Configuration
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
+# https://github.com/cs50/problems/pull/24
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = False
+
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -18,8 +21,10 @@ login_manager = LoginManager(app)
 login_manager.login_view = "login"
 
 from source.views.auth import auth_blueprint
+from source.views.measurement_api import measurement_api_blueprint
 
 app.register_blueprint(auth_blueprint)
+app.register_blueprint(measurement_api_blueprint)
 
 manager.add_command('db', MigrateCommand)
 
