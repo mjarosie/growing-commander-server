2b9178dd1910ad1c4d2c7e8e10aba52c39c8a327
mjarosie <jarosiewicz.maciek@gmail.com>
2018-03-18 17:14:58 +0000

    fix imports, remove unecessary and old code

--- growing_commander_server/scripts/create_db.py
+++ growing_commander_server/scripts/create_db.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from source.main import db
-from source.models import User, ObservationGroup, Measurement
+from growing_commander_server import db
+from growing_commander_server.models import User, Measurement
 
 if __name__ == '__main__':
     db.create_all()
@@ -12,20 +12,8 @@ if __name__ == '__main__':
     db.session.add(user)
     db.session.commit()
 
-    user = User(
-        name='admin',
-        password='admin',
-        is_admin=True
-    )
-    db.session.add(user)
-    db.session.commit()
-
-    obs_group_1 = ObservationGroup(datetime(2017, 6, 25, 17, 15), datetime(2017, 6, 25, 17, 20))
-    db.session.add(obs_group_1)
-    db.session.commit()
-
-    measurement_1 = Measurement("temperature", 25.7, "C", obs_group_1.id)
-    measurement_2 = Measurement("humidity", 29, "%", obs_group_1.id)
+    measurement_1 = Measurement(datetime.now(), "DHT11", "temperature", 25.7, "C")
+    measurement_2 = Measurement(datetime.now(), "DHT11", "humidity", 29, "%")
     db.session.add(measurement_1)
     db.session.add(measurement_2)
     db.session.commit()
