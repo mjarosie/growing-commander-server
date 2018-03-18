f296322652bf30565a658e96dafa93a36272ae68
mjarosie <jarosiewicz.maciek@gmail.com>
2018-03-18 17:11:18 +0000

    add the host parameter

--- growing_commander_server/main.py
+++ growing_commander_server/main.py
@@ -3,6 +3,6 @@ import growing_commander_server.views.app
 
 if __name__ == "__main__":
     if 'CERT_PATH' in app.config and 'KEY_PATH' in app.config:
-        app.run(ssl_context=(app.config['CERT_PATH'], app.config['KEY_PATH']))
+        app.run(host='0.0.0.0', ssl_context=(app.config['CERT_PATH'], app.config['KEY_PATH']))
     else:
-        app.run()
+        app.run(host='0.0.0.0')
