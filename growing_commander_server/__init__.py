ccf1c430cef3777bcb1cc139b96fa32685aecc02
mjarosie <jarosiewicz.maciek@gmail.com>
2018-03-18 17:10:49 +0000

    extract the blueprint from the main view

--- growing_commander_server/__init__.py
+++ growing_commander_server/__init__.py
@@ -18,15 +18,24 @@ migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 
+# Importing and registering blueprints.
 # Imported here due to the circular dependencies.
-# TODO: Figure out if there's a way to make it cleaner.
-from growing_commander_server.views.auth import auth_blueprint
+from growing_commander_server.views.app import app_blueprint
+from growing_commander_server.views.authorization_api import auth_blueprint
 from growing_commander_server.views.measurement_api import measurement_api_blueprint
 
-app.register_blueprint(auth_blueprint)
-app.register_blueprint(measurement_api_blueprint)
+app.register_blueprint(app_blueprint)
+app.register_blueprint(auth_blueprint, url_prefix='/api/v1')
+app.register_blueprint(measurement_api_blueprint, url_prefix='/api/v1')
 
 
 @app.before_request
 def _before_request():
     g.user = current_user
+
+
+from growing_commander_server.models import User
+
+@login_manager.user_loader
+def _user_loader(user_id):
+    return User.query.get(int(user_id))
--- growing_commander_server/views/app.py
+++ growing_commander_server/views/app.py
@@ -1,40 +1,41 @@
-from flask import render_template, request, url_for, flash, redirect
+from flask import render_template, request, url_for, flash, redirect, Blueprint
 from flask_login import login_user, logout_user, login_required
-from growing_commander_server import app
 
-from growing_commander_server.models import User, Measurement
 from growing_commander_server.forms import LoginForm
-from growing_commander_server.tables import UserTable, ObservationGroupTable
+from growing_commander_server.models import User, Measurement
+from growing_commander_server.tables import UserTable, MeasurementsTable
+
+app_blueprint = Blueprint('app_blueprint', __name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app_blueprint.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         form = LoginForm(request.form)
         if form.validate():
             login_user(form.user)
             flash("Successfully logged in as %s." % form.user.name, "success")
-            return redirect(url_for("overview"))
+            return redirect(url_for("app_blueprint.overview"))
     else:
         form = LoginForm()
     return render_template("login_page.html", form=form)
 
 
-@app.route("/logout/")
 @login_required
+@app_blueprint.route("/logout/")
 def logout():
     logout_user()
     flash('You have been logged out.', 'success')
-    return redirect(url_for('login'))
+    return redirect(url_for('app_blueprint.login'))
 
 
-@app.route('/overview')
+@app_blueprint.route('/overview')
 @login_required
 def overview():
     return render_template('homepage.html')
 
 
-@app.route('/users')
+@app_blueprint.route('/users')
 @login_required
 def users():
     all_users = User.query.order_by(User.registered_on.desc()).all()
@@ -42,16 +43,16 @@ def users():
     return render_template('users.html', table=table)
 
 
-@app.route('/measures')
+@app_blueprint.route('/measures')
 @login_required
 def measures():
     tables = []
     measurements = Measurement.query.all()
-    tables.append(ObservationGroupTable(measurements, border=True))
+    tables.append(MeasurementsTable(measurements, border=True))
     return render_template('measures.html', tables=tables)
 
 
-@app.route('/commands')
+@app_blueprint.route('/commands')
 @login_required
 def commands():
-    return render_template('homepage.html')
+    return render_template('api_overview.html')
